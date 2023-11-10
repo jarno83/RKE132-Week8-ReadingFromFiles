5d9baaf7fd95adc3a43aad1470a505484ea95ef4
Jarno Orasmäe <jarnoorasmae@tktk.ee>
2023-11-10 23:31:45 +0200

    add week 8 code

---
+++ Week8EpicGame/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+string folderPath = @"C:\data";
+string heroFile = "heroes.txt";
+string villainsFile = "villains.txt";
+
+string[] heros=File.ReadAllLines(Path.Combine(folderPath, heroFile));
+string[] villains = File.ReadAllLines(Path.Combine(folderPath, villainsFile));
+
+//string[] heros = { "Harry Potter", "Luke Skywalker", "Lara Croft", "Scooby-Doo" };
+//string[] villains = { "Voldemort", "Darth Vader", "Dracula", "Joker", "Sauron" };
+string[] weapon = { "magic wand", "plastic fork", "banana", "wooden sword", "Lego brick" };
+
+string hero = GetRandomValueFromArray(heros);
+string heroWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Today {hero} with {heroWeapon} saves the day!");
+
+string villain = GetRandomValueFromArray(villains);
+string villainWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Today {villain} with {villainWeapon} tries to take over the world!");
+
+static string GetRandomValueFromArray(string[] someArray)
+{
+    Random rnd = new Random();
+    int randomIndex = rnd.Next(0, someArray.Length);
+    string randomStringFromArray = someArray[randomIndex];
+    return randomStringFromArray;
+}
